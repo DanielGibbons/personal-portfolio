02bc4c0428163823a8ac3270dab5f248d453d23a
Daniel Gibbons <d.gibbons1405@gmail.com>
2019-10-26 11:07:53 +0100

    Renamed Posts to Blog. Added favicon. Added functionality for adding new blog posts.

---
+++ data/blog-posts.js
@@ -0,0 +1,42 @@
+var blogPosts = [
+
+	{
+		id: '2019-10-26-00',
+		title: 'First Blog Post',
+		date: '26/10/2019',
+		tags: ['first tag', 'first post']
+	},
+	{
+		id: '2019-10-26-01',
+		title: 'Second Blog Post',
+		date: '26/10/2019',
+		tags: ['second tag', 'second post']
+	},
+	{
+		id: '2019-10-26-01',
+		title: 'Second Blog Post',
+		date: '26/10/2019',
+		tags: ['second tag', 'second post']
+	},
+	{
+		id: '2019-10-26-01',
+		title: 'Second Blog Post',
+		date: '26/10/2019',
+		tags: ['second tag', 'second post']
+	},
+	{
+		id: '2019-10-26-01',
+		title: 'Second Blog Post',
+		date: '26/10/2019',
+		tags: ['second tag', 'second post']
+	},
+	{
+		id: '2019-10-26-01',
+		title: 'Second Blog Post',
+		date: '26/10/2019',
+		tags: ['second tag', 'second post']
+	}
+
+]
+
+module.exports = blogPosts;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 var express = require('express');
-var ViewData = require("../models/view-data.js");
+var ViewData = require('../models/view-data.js');
+var blogPosts = require('../data/blog-posts.js')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { viewData: new ViewData('', 'home') });
+	res.render('index', { viewData: new ViewData('dangibbons.io', 'home') });
 });
 
+/* GET about page. */
 router.get('/about', function (req, res, next) {
 	viewData = new ViewData('About', 'about')
 	res.render('about', { viewData: new ViewData('About', 'about') });
 });
 
-router.get('/posts', function (req, res, next) {
-	res.render('posts', { viewData: new ViewData('Posts', 'posts') });
+/* GET blog page. */
+router.get('/blog', function (req, res, next) {
+	res.render('blog', { viewData: new ViewData('Blog', 'blog'), blogPosts: blogPosts });
 });
 
-
+/* GET projects page. */
 router.get('/projects', function (req, res, next) {
 	res.render('projects', { viewData: new ViewData('Projects', 'projects') });
 });
