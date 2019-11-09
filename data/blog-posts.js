d69d9ba7a479ef5234d68a832cbd1bdf0d900536
Daniel Gibbons <d.gibbons1405@gmail.com>
2019-11-09 17:36:02 +0000

    Remove unfinished blog post.

--- data/blog-posts.js
+++ data/blog-posts.js
@@ -1,14 +1,5 @@
 var blogPosts = {
 
-	BP_2019_11_09_00: {
-		id: 'BP_2019_11_09_00',
-		title: 'Using Prism JS to display code in the Browser',
-		shortTitle: 'Prism JS',
-		date: '09/11/2019',
-		last_modified: '09/11/2019',
-		description: "A guide for setting up your project to use Prism JS with the Autoloader plugin to display code snippets in the browser.",
-		tags: ['Javascript']
-	},
 	BP_2019_11_01_00: {
 		id: 'BP_2019_11_01_00',
 		title: 'Generating a PFX SSL Certificate using GoDaddy',
