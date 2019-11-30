71bb39068c9756df66dbf1829817338d74c4dc48
Daniel Gibbons <d.gibbons1405@gmail.com>
2019-11-30 16:08:15 +0000

    Fix wrong return value from getUrls()

--- data/sitemap.js
+++ data/sitemap.js
@@ -1,39 +1,33 @@
 const baseUrl = 'https://dangibbons.io';
 
+function findUrls() {
+
+	return [
+		'/',
+		'/projects',
+		'/playground',
+		'/blog',
+		'/blog/BP_2019_10_26_00',
+		'/blog/BP_2019_11_01_00'
+	]
+
+}
+
 async function getUrls() {
 
-	return await [
-		{
-			url: '/',
-			lastMod: new Date().toISOString(),
-			changeFreq: 'weekly'
-		},
-		{
-			url: '/projects',
-			lastMod: new Date().toISOString(),
-			changeFreq: 'weekly'
-		},
-		{
-			url: '/playground',
-			lastMod: new Date().toISOString(),
-			changeFreq: 'weekly'
-		},
-		{
-			url: '/blog',
-			lastMod: new Date().toISOString(),
-			changeFreq: 'weekly'
-		},
-		{
-			url: '/blog/BP_2019_10_26_00',
-			lastMod: new Date().toISOString(),
-			changeFreq: 'weekly'
-		},
-		{
-			url: '/blog/BP_2019_11_01_00',
-			lastMod: new Date().toISOString(),
+	var urls = findUrls();
+	var sitemap = [];
+	var date = new Date().toISOString();
+
+	urls.forEach( (url, index) => {
+		sitemap[index] = {
+			url: url,
+			lastMod: date,
 			changeFreq: 'weekly'
 		}
-	];
+	});
+
+	return await sitemap;
 
 }
 
